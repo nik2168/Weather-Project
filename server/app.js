bea6bc4f34448febac9339ee73602457682cad0d
nik2168 <128375663+nik2168@users.noreply.github.com>
2024-07-10 17:04:02 +0530

    deploying

--- server/app.js
+++ server/app.js
@@ -3,12 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const cors = require('cors')
 const corsOptions = require("./constants/config.js");
 
 // routes import
 const userRoutes = require("./routes/user.routes.js");
-
 
 const app = express();
 app.use(cookieParser());
@@ -26,9 +25,7 @@ mongoose
     console.log("Connected to database successfully!");
 
     app.listen(process.env.port, () => {
-      console.log(
-        `Sever is running at port: ${process.env.port} `
-      );
+      console.log(`Sever is running at port: ${process.env.port} `);
     });
 
     app.get("/", (req, res) => {
@@ -40,4 +37,3 @@ mongoose
   });
 
 app.use("/api/v1/user", userRoutes);
-
--- server/constants/config.js
+++ server/constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    "https://nox-chat-frontend.vercel.app/",
+    "https://noxweatherfrontend.vercel.app/",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
